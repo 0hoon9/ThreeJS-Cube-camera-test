1d33ccbd94689193e4bfdaf132a1701a8f66f201
0hoon9 <0hoon9@hivelab.co.kr>
2021-12-12 05:04:43 +0900

    -module 분리

---
+++ src/js/Cube.js
@@ -0,0 +1,82 @@
+import * as THREE from '../../node_modules/three';
+import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls';
+
+class Cube {
+  constructor(cubePath, texturePath) {
+    this.scene = null;
+    this.camera = null;
+    this.renderer = null;
+    this.controls = null;
+
+    this.cubePath = cubePath;
+    this.texturePath = texturePath;
+    this.textureCube = null;
+    this.textureBox = null;
+    this.sphereMesh = null;
+    this.sphereMaterial = null;
+
+    this.init();
+    this.render();
+  }
+
+  init() {
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth, window.innerHeight, 1, 100000);
+    this.camera.position.set(0, 0, 1000);
+
+    const ambient = new THREE.AmbientLight( 0xffffff );
+    this.scene.add(ambient);
+
+    const loader = new THREE.CubeTextureLoader();
+    // loader.setPath('images/textures/');
+    loader.setPath(this.cubePath);
+
+    // textureCube = loader.load([ 'left.png', 'right.png', 'top.png', 'bottom.png', 'front.png', 'back.png' ]);
+    this.textureCube = loader.load( [ 'left.jpg', 'right.jpg', 'top.jpg', 'bottom.jpg', 'back.jpg', 'front.jpg' ] );
+    this.textureCube.encoding = THREE.sRGBEncoding;
+
+    const textureLoader = new THREE.TextureLoader();
+
+    this.textureBox = textureLoader.load(this.texturePath);
+    this.textureBox.mapping = THREE.EquirectangularReflectionMapping;
+    this.textureBox.encoding = THREE.sRGBEncoding;
+
+    this.scene.background = this.textureCube;
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    document.body.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.minDistance = 500;
+    this.controls.maxDistance = 2500;
+
+    window.addEventListener( 'resize', this.onWindowResize );
+    this.onWindowResize();
+  }
+
+  render() {
+    const scene = this.scene;
+    const camera = this.camera;
+    const renderer = this.renderer;
+
+    function frame() {
+      camera.lookAt(scene.position);
+      renderer.render(scene, camera);
+      requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
+  }
+
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+}
+
+export default Cube;
--- src/main.js
+++ src/main.js
@@ -1,69 +1,10 @@
-import * as THREE from '../node_modules/three';
-import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls';
-
-let scene = null;
-let camera = null;
-let renderer = null;
-let controls = null;
-
-let textureCube, textureBox;
-let sphereMesh, sphereMaterial;
+import Cube from './js/Cube.js';
 
 init();
-animate();
 
 function init() {
-    scene = new THREE.Scene();
+    const path = 'images/bridge/';
+    const texturePath ='images/textures/studio.jpg';
 
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth, window.innerHeight, 1, 100000);
-    camera.position.set(0, 0, 1000);
-
-    const ambient = new THREE.AmbientLight( 0xffffff );
-    scene.add(ambient);
-
-    const loader = new THREE.CubeTextureLoader();
-    // loader.setPath('images/textures/');
-    loader.setPath('images/bridge/');
-
-    // textureCube = loader.load([ 'left.png', 'right.png', 'top.png', 'bottom.png', 'front.png', 'back.png' ]);
-    textureCube = loader.load( [ 'left.jpg', 'right.jpg', 'top.jpg', 'bottom.jpg', 'back.jpg', 'front.jpg' ] );
-    textureCube.encoding = THREE.sRGBEncoding;
-
-    const textureLoader = new THREE.TextureLoader();
-
-    textureBox = textureLoader.load('images/textures/studio.jpg');
-    textureBox.mapping = THREE.EquirectangularReflectionMapping;
-    textureBox.encoding = THREE.sRGBEncoding;
-
-    scene.background = textureCube;
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.outputEncoding = THREE.sRGBEncoding;
-    document.body.appendChild(renderer.domElement);
-
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.minDistance = 500;
-    controls.maxDistance = 2500;
-
-    window.addEventListener( 'resize', onWindowResize );
-    onWindowResize();
-}
-
-function render() {
-    camera.lookAt( scene.position );
-    renderer.render( scene, camera );
-}
-
-function animate() {
-    requestAnimationFrame( animate );
-    render();
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    new Cube(path, texturePath);
 }
